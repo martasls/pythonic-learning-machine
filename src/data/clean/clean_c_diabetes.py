0ff30b645aa3c4946e4a12abbdd7f99d022b4adb
Marta <m20170451@novaims.unl.pt>
2018-11-29 01:33:04 +0000

    updated paths

--- src/data/clean/clean_c_diabetes.py
+++ src/data/clean/clean_c_diabetes.py
@@ -3,23 +3,25 @@ import sys
 import pandas as pd 
 
 #get working directory 
-#wd = os.getcwd() 
-
-#print(wd)
-path = 'C:\\Users\\Marta\\Documents\\GitHub\\pythonic-learning-machine\\data_sets\\'
-wd = os.chdir(path)
+wd = os.getcwd() 
+wd = os.path.realpath('..')
+print(wd)
+folder = os.path.join(wd, 'data_sets')
+print(folder)
+#wd = os.chdir(path)
 
 #print(os.listdir())
 
-c_diabetes = pd.read_csv('01_raw\\c_diabetes\\data_set.csv', header=None)
-#print(c_diabetes.head())
+data_set = os.path.join(folder, '01_raw', 'c_diabetes', 'data_set.csv')
+c_diabetes = pd.read_csv(data_set, header=None)
+print(c_diabetes.head())
 
 outname = 'c_diabetes.csv'
 outdir = '02_cleaned'
-if not os.path.exists(outdir):
+if not os.path.join(folder, outdir):
     os.mkdir(outdir)
 
-fullname = os.path.join(outdir, outname)
-
+fullname = os.path.join(folder, outdir, outname)
+print(fullname)
 #save data set to csv
 c_diabetes.to_csv(fullname)
